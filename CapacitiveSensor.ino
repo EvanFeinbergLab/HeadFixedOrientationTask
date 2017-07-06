ad0c1a229d8851dbc19d6a21ba844caf345c0146
hzubair <humza.n.zubair@gmail.com>
2017-07-06 14:28:19 -0700

    Add files via upload
    New timer function, so matlab does not lag behind.

---
+++ CapacitiveSensor.ino
@@ -0,0 +1,27 @@
+#include <CapacitiveSensor.h>
+
+CapacitiveSensor cs_7_6 = CapacitiveSensor(7, 6);  // 1M resistor between pins 7 & 6, pin 6 is sensor pin
+
+unsigned long previousTime = 0;
+const long interval = 10;
+
+void setup()                    
+{
+   //cs_7_6.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+}
+
+void loop()                    
+{
+    unsigned long currentTime=millis();
+    int total = cs_7_6.capacitiveSensor(5);
+
+    if (currentTime-previousTime >= interval) {
+       previousTime=currentTime;    
+       Serial.print(total);                  // print sensor output 1
+       Serial.println("\t");
+    }
+
+
+
+}
