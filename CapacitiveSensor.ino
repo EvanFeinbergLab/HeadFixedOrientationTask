d5b184ce8397c05248b298600d558e2c86e87353
Alison Kim <alison.y.kim@berkeley.edu>
2017-07-10 10:50:57 -0700

    Add files via upload

--- CapacitiveSensor.ino
+++ CapacitiveSensor.ino
@@ -1,34 +1,25 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs_7_6 = CapacitiveSensor(7, 6);  // 1M resistor between pins 7 & 6, pin 6 is sensor pin
+CapacitiveSensor cs_10_9 = CapacitiveSensor(10, 9);  // 1M resistor between pins 10 & 9, pin 9 is sensor pin
 
 unsigned long previousTime = 0;
 const long interval = 10;
 
 void setup()                    
 {
-   //cs_7_6.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   //cs_10_9.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
    Serial.begin(9600);
 }
 
 void loop()                    
 {
     unsigned long currentTime=millis();
-    int total = cs_7_6.capacitiveSensor(5);
+    int total = cs_10_9.capacitiveSensor(5);
 
     if (currentTime-previousTime >= interval) {
        previousTime=currentTime;    
        Serial.print(total);                  // print sensor output 1
        Serial.println("\t");
     }
-
-
-
-}
-    int total = cs_7_6.capacitiveSensor(5);
-
-    Serial.print(total);                  // print sensor output 1
-    Serial.println("\t");
-
-    delay(10);                             // arbitrary delay to limit data to serial port 
+    
 }
